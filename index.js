9828ff0486a9db357c620115439b1e944c6bde14
Misha Krasnonos <mikhail.k@kitelytech.com>
2023-05-27 19:14:26 +0300

    up

--- index.js
+++ index.js
@@ -11,41 +11,41 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api/superheroes', superherosRouter);
-// app.get('/', (req, res) => {
-//   res.end(`
-//     <div>
-//         <nav>
-//             <ul>
-//                 <li>
-//                     <a href="/">Home</a>
-//                 </li>
-//                 <li>
-//                     <a href="/about">About</a>
-//                 </li>
-//             </ul>
-//         </nav>
-//     </div>
-//     <h1>Home page</h1>
-//     `)
-// })
-// app.get('/about', (req, res) => {
-//   res.end(`
-//     <div>
-//         <nav>
-//             <ul>
-//                 <li>
-//                     <a href="/">Home</a>
-//                 </li>
-//                 <li>
-//                     <a href="/about">About</a>
-//                 </li>
-//             </ul>
-//         </nav>
-//     </div>
-//     <h1>About page</h1>
-//     `)
-// })
+// app.use('/api/superheroes', superherosRouter);
+app.get('/', (req, res) => {
+  res.end(`
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <h1>Home page</h1>
+    `);
+});
+app.get('/about', (req, res) => {
+  res.end(`
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <h1>About page</h1>
+    `);
+});
 
 app.listen(PORT, () => {
   console.log(`Server has been started on ${PORT} port`);
