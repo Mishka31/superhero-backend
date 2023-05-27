ca7a0b55d8b56a6527a2c2da989bfec4cc5cb48d
Misha Krasnonos <mikhail.k@kitelytech.com>
2023-05-27 19:19:10 +0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { asyncTryCatch } = require('./middleware');
+const { ctrl } = require('./controllers');
 
 const PORT = process.env.PORT || 80;
 
@@ -12,40 +14,41 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // app.use('/api/superheroes', superherosRouter);
-app.get('/', (req, res) => {
-  res.end(`
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <h1>Home page</h1>
-    `);
-});
-app.get('/about', (req, res) => {
-  res.end(`
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <h1>About page</h1>
-    `);
-});
+app.get('/', asyncTryCatch(ctrl.getAll));
+// app.get('/', (req, res) => {
+//   res.end(`
+//     <div>
+//         <nav>
+//             <ul>
+//                 <li>
+//                     <a href="/">Home</a>
+//                 </li>
+//                 <li>
+//                     <a href="/about">About</a>
+//                 </li>
+//             </ul>
+//         </nav>
+//     </div>
+//     <h1>Home page</h1>
+//     `);
+// });
+// app.get('/about', (req, res) => {
+//   res.end(`
+//     <div>
+//         <nav>
+//             <ul>
+//                 <li>
+//                     <a href="/">Home</a>
+//                 </li>
+//                 <li>
+//                     <a href="/about">About</a>
+//                 </li>
+//             </ul>
+//         </nav>
+//     </div>
+//     <h1>About page</h1>
+//     `);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server has been started on ${PORT} port`);
