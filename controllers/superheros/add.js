665b3bad8139a7f225536c7be1518f76ec098759
Misha Krasnonos <mikhail.k@kitelytech.com>
2023-05-27 20:10:20 +0300

    up

--- controllers/superheros/add.js
+++ controllers/superheros/add.js
@@ -5,14 +5,13 @@ const path = require('path');
 const pathAvatars = path.join(__dirname, '../../public/images');
 
 const addSuperhero = async (req, res) => {
-  console.log(req);
   const { path: tempPath, originalname } = req.file;
-  const resultDir = path.join(pathAvatars, originalname);
-  await fs.rename(tempPath, resultDir);
-  const imageUrl = path.join('/images', originalname);
-  const newHero = { ...req.body, imageUrl };
+  // const resultDir = path.join(pathAvatars, originalname);
+  // await fs.rename(tempPath, resultDir);
+  // const imageUrl = path.join('/images', originalname);
+  // const newHero = { ...req.body, imageUrl };
   // const result = await Superhero.create(newHero);
-  res.status(201).json({ status: 'Succes saved file', code: 201 });
+  res.status(201).json({ status: 'Succes saved file', code: 201, data: { name: originalname } });
 };
 
 module.exports = addSuperhero;
