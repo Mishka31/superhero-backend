749b88fabf84576cbde8f1d084ea00be6270d3f3
Misha Krasnonos <mikhail.k@kitelytech.com>
2023-05-28 18:23:54 +0300

    ads

--- controllers/superheros/getById.js
+++ controllers/superheros/getById.js
@@ -1,12 +1,22 @@
-const { NotFound } = require('http-errors')
-const { Superhero } = require('../../model')
+const { db } = require('../../firebase/config');
 
 const getById = async (req, res) => {
-  const { id } = req.params
-  const result = await Superhero.findById(id)
-  if (!result) {
-    throw new NotFound(`Not found id = ${id}`)
+  const { id } = req.query;
+  try {
+    const doc = await db.collection('superheroes').doc(id).get();
+    if (!doc.exists) {
+      return res.status(404).json({ message: 'Superhero not found' });
+    }
+
+    const superheroData = doc.data();
+    const superhero = {
+      id: doc.id,
+      ...superheroData,
+    };
+    res.json({ message: 'Superhero found', data: superhero });
+  } catch (error) {
+    console.error('Error fetching superhero from Firestore:', error);
+    res.status(500).json({ message: 'Failed to fetch superhero' });
   }
-  res.json({ message: 'template message', code: 200, data: { result } })
-}
-module.exports = getById
+};
+module.exports = getById;
