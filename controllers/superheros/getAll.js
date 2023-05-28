d8c3f3cb1d9981984cc5082eb0a2e6e8311cd980
Misha Krasnonos <mikhail.k@kitelytech.com>
2023-05-28 17:16:50 +0300

    asd

--- controllers/superheros/getAll.js
+++ controllers/superheros/getAll.js
@@ -3,14 +3,14 @@ const { db } = require('../../firebase/config');
 
 const getAll = async (_, res) => {
   try {
-    const snapshot = await db.collection('superheroes').get();
+    // const snapshot = await db.collection('superheroes').get();
 
-    const superheroes = [];
-    snapshot.forEach((doc) => {
-      superheroes.push(doc.data());
-    });
+    // const superheroes = [];
+    // snapshot.forEach((doc) => {
+    //   superheroes.push(doc.data());
+    // });
 
-    res.json({ message: 'Get all heroes', code: 200, data: superheroes });
+    res.json({ message: 'Get all heroes', code: 200, data: { Test: 'test' } });
   } catch (error) {
     res.status(500).json({ status: 'Error', code: 500, message: 'Failed to get heroes' });
   }
