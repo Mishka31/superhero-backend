38fc26176aa9357047940c88655f037f69eb181e
Misha Krasnonos <muzishutka7@gmail.com>
2023-05-28 18:42:21 +0300

    asd

--- controllers/superheros/getById.js
+++ controllers/superheros/getById.js
@@ -7,7 +7,7 @@ const getById = async (req, res) => {
     if (!doc.exists) {
       return res.status(404).json({ message: 'Superhero not found' });
     }
-
+    // console.log('');
     const superheroData = doc.data();
     const superhero = {
       id: doc.id,
